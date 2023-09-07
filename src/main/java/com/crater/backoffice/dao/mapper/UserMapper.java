84e4e07e70921e2b8b03ca249b34983a6e1d97d1
曉郁 <s19970523s@gmail.com>
2023-09-07 22:47:38 +0800

    回復 insert 數量

--- src/main/java/com/crater/backoffice/dao/mapper/UserMapper.java
+++ src/main/java/com/crater/backoffice/dao/mapper/UserMapper.java
@@ -7,5 +7,5 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMapper {
     UserDetailInfoDto queryUserInfoByUserId(String userId);
-    void insertUser(InsertUserDbDto insertUserDbDto);
+    int insertUser(InsertUserDbDto insertUserDbDto);
 }
