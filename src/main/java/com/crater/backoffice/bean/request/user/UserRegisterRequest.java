e2232a71dea9a545ef4f0071a109c332653c1206
曉郁 <s19970523s@gmail.com>
2023-09-07 22:42:20 +0800

    完善 swagger

--- src/main/java/com/crater/backoffice/bean/request/user/UserRegisterRequest.java
+++ src/main/java/com/crater/backoffice/bean/request/user/UserRegisterRequest.java
@@ -1,8 +1,9 @@
 package com.crater.backoffice.bean.request.user;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.lang.NonNull;
 
-public record UserRegisterRequest(@Schema(example = "ken", description = "使用者id") String userId,
-                                  @Schema(example = "abc@gmail.com", description = "email") String email,
-                                  @Schema(example = "xxxxx", description = "使用者的密碼，目前使用明碼將來會用BCrypt傳進來，") String password) {
+public record UserRegisterRequest(@Schema(example = "ken", description = "使用者id") @NonNull String userId,
+                                  @Schema(example = "abc@gmail.com", description = "email") @NonNull String email,
+                                  @Schema(example = "xxxxx", description = "使用者的密碼，目前使用明碼將來會用BCrypt傳進來，") @NonNull String password) {
 }
